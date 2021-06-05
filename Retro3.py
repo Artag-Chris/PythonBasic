56fb74d11e2729d88a9d15635c439aadfff19e65
Christian <scristxyz@gmail.com>
2021-06-05 16:08:26 -0500

    avance del reto 3 y uso de diccionarios

--- Retro3.py
+++ Retro3.py
@@ -4,9 +4,9 @@ equipoParticipante=[]
 provicional=[]
 #lista de fechas
 fechas=[]
-#se debe crear una forma que asi gane 0 puntos en una fecha el equipo perdedor gane un [0]
+
+#encontrar la falla en el registrar equipos que solo permite ingresar 2 seguidos
 #organizar front-end
-compras=[]
 isActivateMenu=True
 isAddEquipo=True
 isAddProveedor=True
@@ -14,47 +14,52 @@ isAddjugador=True
 op=0
 while isActivateMenu==True:
     #menu principal hay que modificarlo
-    print("1.Ingresar Equipo o Jugador\n2.Ingresar Datos de cada partido\n3.Buscar jugador por Codigo del equipo\n4.Tabla de posiciones\n5.Listar Equipos")
+    print("1.Ingresar Equipo o Jugador\n2.Ingresar Datos de cada partido\n3.Buscar jugador por Codigo del equipo\n4.Tabla de posiciones\n5.Listar Equipos\n6.Ver todas las fechas\n7.Salir")
     op=int(input())
     if op==1:
         isAddEquipo=True
         equipoParticipante=[]
         while isAddEquipo==True:
-            #submenu que agrege para facilitar el registro de equipos y jugadores         
+            #submenu que agrege para facilitar el registro de equipos y jugadores por separado        
             opc=0
             print("Selecione una opcion \n1. registrar un equipo al torneo\n2. ingresar un jugador a la plantilla de jugadores\n3. Salir")
             opc=int(input())
             if opc==1:
+                isAddEquipo=True
                 codigo=input("Ingrese el codigo del Equipo :")
                 nombre=input("Ingrese el nombre del Equipo :")
                 pais=input("Ingrese la confederacion a la que pertenece el Equipo :")
                 plantel=int(input("Ingrese el  numero del plantel del Equipo : "))
                 #la penultima lista que se crea es donde vendra la informacion de la plantilla 
                 #la ultima lista es donde se guardara la fecha y los puntos ganados
+                #el diccionario es donde guardare datos de partidos y numeros
                 equipoParticipante=[]
-                equipoParticipante=[codigo.upper(),nombre.upper(),pais.upper(),plantel,[],[]]
+                equipoParticipante=[codigo.upper(),nombre.upper(),pais.upper(),plantel,[],[],{}]
                 equipo.append(equipoParticipante)
-                #cambiar este menu por ingresar otro equipo
-                rta=input("Desea Ingresar un jugador del equipo S o N : ")
+                rta=input("Desea Ingresar un equipo al torneo S o N : ")
                 if rta.upper()=="S":
-                    isAddJugador=True
-                    nombre=input("Ingrese Nombre del jugador : ")
-                    posicion=input("Ingrese posicion del jugador : ")
-                    provicional.append([nombre,posicion])
-                    provicional=[]
-                    rta=input("Desea Ingresar otro jugador S o N : ")
+                    isAddEquipo=True
+                    codigo=input("Ingrese el codigo del Equipo : ")
+                    nombre=input("Ingrese el nombre del Equipo : ")
+                    pais=input("Ingrese la confederacion a la que pertenece el Equipo :")
+                    plantel=int(input("Ingrese el  numero del plantel del Equipo : "))
+                    equipoParticipante=[]
+                    equipoParticipante=[codigo.upper(),nombre.upper(),pais.upper(),plantel,[],[],{}]
+                    equipo.append(equipoParticipante)
+                    rta=input("Desea Ingresar otro Equipo S o N : ")
                     if rta.upper()=="S":
-                        isAddJugador=True
+                        isAddEquipo=True
                     elif rta.upper()=="N":
-                        isAddJugador=False
+                        isAddEquipo=False
                     else:
                         print("Respuesta no valida")
-                        isAddJugador=True
+                        isAddEquipo=True
                 elif rta.upper()=="N":
-                    isAddJugador=False
+                    isAddEquipo=False
                 else:
                     print("Respuesta no valida")
-                    isAddJugador=True
+                    isAddEquipo=True
+                   
             elif opc==2:
                 palabra=input("Ingrese el codigo del equipo a Buscar : ")
                 isAddJugador=True
@@ -65,9 +70,10 @@ while isActivateMenu==True:
                             nombre=input("Ingrese nombre del jugador : ")
                             posicion=input("Ingrese posicion del jugador : ")
                             provicional.extend([nombre,posicion])
+                            #item[4] hace referencia al 5 puesto de la lista donde se encuentre el id del equipo
                             item[4].extend(provicional)
                             provicional=[]
-                            print(equipoParticipante)
+                            print(item[4])
                             rta=input("Desea Ingresar otro Jugador S o N : ")
                             if rta.upper()=="S":
                                 isAddJugador=True
@@ -86,6 +92,7 @@ while isActivateMenu==True:
                 print("Respuesta no valida")
                 isAddJugador=True
     elif op==2:
+        #menu donde registrare los puntos par cada equipo y las fechas quedaran registradas en su propia gran lista
         fecha=input("Ingrese la fecha en orden numerico : ")
         equipoA=input("Ingrese el id del equipo que juega como en casa : ")
         golesA=input("Ingrese la cantidad de goles que lograron : ")
@@ -98,69 +105,55 @@ while isActivateMenu==True:
             for item in equipo:
                 if equipoA.upper() in item:
                     item[5].extend([3])
+                   
+                  
+            #me invente este ciclo por separado para buscar la id del equipo perdedor y colocarle su +0
+            for item in equipo:
+                if equipoB.upper() in item:
+                    item[5].extend([0])
                     print(equipo)
         elif golesA < golesB:
             print(f" el equipo ganador es equipo {equipoB} con {golesB} recibira +3 puntos  ")
-            Fechas=[fecha,equipoA,golesA,equipoB,golesB]
+            jornada=[fecha,equipoA.upper(),golesA,equipoB.upper(),golesB]
+            fechas.append(jornada)
             for item in equipo:
                 if equipoB.upper() in item:
                     item[5].extend([3])
-                    print(equipo)
-        elif golesA == golesB:
-            print(f" los equipos empataron y ambos recibiran +1 puntos ")
-            Fechas=[fecha,equipoA,golesA,equipoB,golesB] 
-            for item in equipo:
-                if equipoB.upper() and equipoA in item:
-                    item[5].extend([1])
-                    print(equipo)
-        #fechas=[fecha,equipoA,golesA,equipoB,golesB]
-        
-        print(fechas)
+                   
 
-        
+                for item in equipo:
+                    if equipoA.upper() in item:
+                        item[5].extend([0])
+                        print(equipo)
+                
+        elif golesA == golesB:
+            print(f" los equipos empataron y ambos recibiran +1 punto ")
+            jornada=[fecha,equipoA.upper(),golesA,equipoB.upper(),golesB]
+            fechas.append(jornada)
+            for item in equipo:
+                if equipoB.upper() in item:
+                    item[5].extend([1])
                     
+                
+            for item in equipo:
+                if equipoA.upper() in item:
+                    item[5].extend([1])
+      
+                    print(equipo)
+
+        print(fechas)             
     elif op==3:
         palabra=input("Ingrese el codigo del equipo a Buscar : ")
         for item in equipo:
             if palabra.upper() in item:
-                print(f"lista de jugadores para {palabra.upper()} {item[4]  } ")
+                print(f"lista de jugadores para {palabra.upper()} {item[4]} ")
                 
     elif op==4:
-        isBuying=True
-        while isBuying==True: 
-            palabra=input("Ingrese el codigo del producto a Comprar : ")
-            cantidad=int(input("Ingrese Cantidad a Comprar : "))
-            provid=input("Ingrese el codigo del Proveedor : ")
-            valorc=float(input("Ingrese Cantidad a Comprar : "))
-            isFound=False
-            for producto in equipo:
-                if palabra.upper() in producto:
-                    for prov in producto[4]:
-                        if provid in prov:
-                            isFound=True
-                    if isFound==True:
-                        buy=([producto[0],producto[1],cantidad,valorc,provid])
-                        if producto[3]!=valorc:
-                            producto[3]=valorc
-                        compras.append(buy)
-                    else:
-                        print("No se puede comprar")
-            rta=input("Desea registrar otra compra S o N")
-            if rta.upper()=="S":
-                isBuying=True
-            else:
-                isBuying=False          
+        print("En construccion")     
     elif op==5:
-        for item in equipo:
-            print(f"Codigo : {item[0]} Nombre Producto: {item[1]} Cantidad Disponible : {item[2]} Valor Unitario : {item[3]}",end="\n")
+        print("En construccion")
     elif op==6:
-        totalcompra=0
-        header=["Codigo","Nombre del producto","Cantidad","Valor Unit","Subtotal"]
-        print(f"{header[0]} {header[1]} {header[2]} {header[3]} {header[4]}")
-        for buy in compras:
-            print(f"{buy[0].ljust(6)} {buy[1].ljust(19)} {str(buy[2]).ljust(8)} {str(buy[3]).ljust(10)} {(buy[2]*buy[3])}",end="\n")
-            totalcompra=totalcompra+(buy[2]*buy[3])
-        print(f"Total de la Compra : {totalcompra}")      
+        print(fechas)
     elif op==7:
         rta=input("Desea Abandonar el Programa S o N : ")
         if rta.upper()=="S":
--- diciagenda.py
+++ diciagenda.py
@@ -1,5 +1,8 @@
 agenda=[]
+#colocar ciudades y direcciones sin lista 
+#como funciona la creacion y el update de un diccionario?
 dirContacto={}
+#data={0:{"idContacto" : "123" ,"nombre" : "emmanuel","email":"dfe", "movil" :"3434", "ubicacion":{},"ciudadNac":"Buc", "edad": "18"}}
 isMenuActive=True
 isAddItem=True
 op=0
@@ -20,6 +23,8 @@ while isMenuActive==True:
             nombre=input("Nombre del contacto : ")
             email=input("E-mail del contacto : ")
             movil=input("Tel Movil del contacto : ")
+            #no hace nada
+            valor=len(dirContacto)
             contacto={
                 "idContacto":idContacto,
                 "nombre": nombre,
@@ -27,29 +32,34 @@ while isMenuActive==True:
                 "movil": movil,
                 "ubicacion":{}
             }
-            agenda.append(contacto)
+            dirContacto.update({idContacto:contacto})
+            #agenda.append(contacto)
             rta=input("Desea ingresar otro contacto S o N")
             if rta.upper()=="S":
                 isAddItem=True
             elif rta.upper()=="N":
                 isAddItem=False
     elif op==2:
-        print(agenda)
+        #dirContacto.update(data)
+        print(dirContacto)
         #palabra=input("ingrese el Id a buscar")
         #for contacto in agenda:
             #for Key, valor in agenda.items():
                 #if palabra in valor:
                     #print("Ok")
     elif op==3:
-        palabra=input("ingrese el Id a buscar : ")
-        for contacto in agenda:
-            for key, valor in contacto.items():
-                if palabra in valor:
-                    #estudiar losiguiente
-                    valor=len(contacto["ubicacion"])
-                    ciudad=input("Ingrese la ciudad : ")
-                    direccion=input("Ingrese la Direccion : ")
-                    contacto["ubicacion"].update({valor:{"ciudad":ciudad, "direccion": direccion }})
+        palabra=input("Ingrese Id a buscar : ")
+        #los diccionarios se recorren por key" llave" y value"valor" en el nombre del directorio con el .items
+        #ese valorc es una variable de contador para los value "valores podria tener cualquier nombre" va cambiando de valor 
+        for key, valorc in dirContacto.items():
+            #si la key "llave" es igual a palabra(input) hace lo siguiente
+            if str(key) == palabra:
+                #este valor toma un numero que es la longitud que tiene el value de "ubicacion" si esta vacio sera 0
+                valor=len(valorc["ubicacion"])
+                ciudad=input("Ingrese ciudad para registrar : ")
+                direccion=input("Ingrese direccion para registrar : ")
+                #aqui encuentra "ubicacion" dentro del diccionario y le hace un update le agrega la nueva data
+                valorc["ubicacion"].update({valor :{"ciudad" : ciudad, "direccion" : direccion}})
     elif op==4:
         rta=input("Desea Salir del programa S o N ")
         if rta.upper()=="S":
